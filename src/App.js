4bc3f253971bc358b863794fb5d62254ed7c220a
thineshrajan <thinesh.rj@gmail.com>
2024-04-07 14:33:56 +0530

    add css changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import DataTable from './components/DataTable';
+import './App.css'
 
 const App = () => {
   const [user, setUser] = useState(null);
@@ -10,7 +11,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<h1>Welcome to the App</h1>} />
+        <Route path="/" element={<h1>Welcome to the Organization</h1>} />
         <Route path="/login" element={<Login setUser={setUser} />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/data" element={<DataTable user={user} />} />
